23905552cf3362386ffbaa9195bd2040262fc1c4
Daniel Himsani <Danielhimsani@gmail.com>
2025-06-14 14:36:03 +0000

    change the דנשבסקי to דנישבסקי

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -18,10 +18,11 @@ export default function MainLayout({
       <footer className="py-6 md:px-8 md:py-0 bg-background border-t hidden md:block">
         <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
           <p className="text-balance text-center text-sm leading-loose text-muted-foreground md:text-right">
-            נבנה באהבה ע״י דניאל הימסני וסטיבן דנשבסקי. כל הזכויות שמורות © {new Date().getFullYear()}
+            נבנה באהבה ע״י דניאל הימסני וסטיבן דנישבסקי. כל הזכויות שמורות © {new Date().getFullYear()}
           </p>
         </div>
       </footer>
     </div>
   );
 }
+
