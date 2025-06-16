943317c78fd676b717ad714a060cdb0dd8fda6b8
Daniel Himsani <Danielhimsani@gmail.com>
2025-06-16 16:54:11 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Toaster } from "@/components/ui/toaster";
 import './globals.css';
 import { cn } from '@/lib/utils';
 import { ThemeInitializer } from '@/components/layout/ThemeInitializer';
-// Removed import for GlobalSettingsDialog: import { GlobalSettingsDialog } from '@/components/layout/GlobalSettingsDialog'; 
+// Removed import for GlobalSettingsDialog
 
 const rubik = Rubik({
   subsets: ['latin', 'hebrew'],
@@ -31,7 +31,7 @@ export default function RootLayout({
       </head>
       <body className={cn('font-body antialiased', rubik.variable)}>
         <ThemeInitializer />
-        {/* Removed GlobalSettingsDialog instance: <GlobalSettingsDialog /> */}
+        {/* Removed GlobalSettingsDialog instance */}
         {children}
         <Toaster />
       </body>
